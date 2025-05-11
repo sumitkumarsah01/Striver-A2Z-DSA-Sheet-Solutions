eb53732d621fd3f2c02932ab39bd71dc527ca681
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-05-11 16:48:51 +0530

    Area Of Circle

---
+++ DAY 1 Basic/areaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float r=sc.nextFloat();
+        double pi=3.14;
+        System.out.println(pi*r*r);
+    }
+    
+}
