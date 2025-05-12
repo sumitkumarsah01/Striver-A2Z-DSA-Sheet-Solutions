3ca44c2078d76dd21c13457f5e0ed246cdc64cda
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-05-12 13:43:19 +0530

    Find the largest number

---
+++ Step 1 Basic/findLargestOfThreeNum.java
@@ -0,0 +1,18 @@
+public class findLargestOfThreeNum {
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        int c=5;
+        if(a>b && a>c){
+            System.out.println("a is the largest Number"+a);
+        }
+        else if(b>a && b>c){
+              System.out.println("b is the largest Number"+b);
+
+        }
+        else{
+            System.out.println("c is the largest Number"+c);
+        }
+    }
+    
+}
