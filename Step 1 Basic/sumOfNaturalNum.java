4bcb74b13a140d67021793546c10e6edcbfd8323
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-05-13 15:03:24 +0530

    Sum Of Natural Num

---
+++ Step 1 Basic/sumOfNaturalNum.java
@@ -0,0 +1,12 @@
+public class sumOfNaturalNum {
+    public static void main(String[] args) {
+        int num=1;
+        int sum=0;
+        while(num<=5){
+            sum+=num;
+            num++;
+        }
+        System.out.println(sum);
+    }
+    
+}
