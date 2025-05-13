12159376b54e1dcf1046a952e5b112bbee7cc9a1
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-05-13 15:02:34 +0530

    Print Natural Num

---
+++ Step 1 Basic/number1to10.java
@@ -0,0 +1,10 @@
+public class number1to10 {
+    public static void main(String[] args) {
+        int number=1;
+        while(number<=10){
+            System.out.println(number);
+            number++;
+        }
+    }
+    
+}
